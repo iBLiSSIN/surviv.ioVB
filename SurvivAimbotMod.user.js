2b0ebe702f260a2f0afad879b08d7980327675cb
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-04-06 19:52:48 +0700

    Update SurvivAimbotMod.user.js

--- SurvivAimbotMod.user.js
+++ SurvivAimbotMod.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name     	Surviv.io mods by VN BPM
+// @name     	Surviv.io AIMBOT mods by VN BPM
 // @description Support aimbot for surviv.io
 // @namespace    https://greasyfork.org/scripts/419689-surviv-io-aim-assist-by-vn-bpm/code/Survivio%20aim%20assist%20by%20VN%20BPM.user.js
 // @version  	2.0
