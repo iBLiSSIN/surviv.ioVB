01eefc2c686a611e853657dbd4d8e05418e77cee
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-04-06 19:47:34 +0700

    Create SurvivMods.user.js

---
+++ SurvivMods.user.js
@@ -0,0 +1,52 @@
+==UserScript==
+// @name     	Surviv.io mods by VN BPM
+// @description Support aimbot for surviv.io
+// @namespace    https://greasyfork.org/scripts/419689-surviv-io-aim-assist-by-vn-bpm/code/Survivio%20aim%20assist%20by%20VN%20BPM.user.js
+// @version  	2.0
+// @grant    	unsafeWindow
+// @author       VN BPM
+// @match        *://surviv.io/*
+// @match        *://surviv2.io/*
+// @match        *://2dbattleroyale.com/*
+// @match        *://2dbattleroyale.org/*
+// @match        *://piearesquared.info/*
+// @match        *://thecircleisclosing.com/*
+// @match        *://archimedesofsyracuse.info/*
+// @match        *://secantsecant.com/*
+// @match        *://parmainitiative.com/*
+// @match        *://nevelskoygroup.com/*
+// @match        *://kugahi.com/*
+// @match        *://chandlertallowmd.com/*
+// @match        *://ot38.club/*
+// @match        *://kugaheavyindustry.com/*
+// @match        *://drchandlertallow.com/*
+// @match        *://rarepotato.com/*
+// @require      http://code.jquery.com/jquery-3.3.1.js 
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+var injector = new Object();
+injector.loader = "Load";
+injector.load = () => {
+	Math.round(4.9);
+Math.round(4.7);    
+Math.round(4.4);   
+Math.round(4.2);  
+Math.round(-4.2); 
+Math.sign(-4);  
+Math.sign(0);    
+Math.sign(4);
+injector.load = "Surviv.iofCopyRightJustinNICkDon'TsTeal2018";
+//pog injectedd 2021 patchhhh
+}
+ 
+}
+ 
+if(false) {
+	injector.load();
+	//inject into surviv.io
+}
+ 
+//aimbot script
+var _0x59cb=['hostname','210713RxhMXN','<H1>YOU\x20HAVE\x20BEEN\x20DETECTED\x20HACKING\x20BY\x20NEW\x20SURVIV.IO\x20ANTICHEAT</H1>','108476Ovuvws','994468SKDJsH','write','1rPqpHN','67247jSAgsF','10RrTbaL','303067LnEOEN','surviv.io','location','991678gFNAim','197DxaQQi','3792XUoemz'];var _0x42ac=function(_0x10da29,_0x58bc8e){_0x10da29=_0x10da29-0xff;var _0x59cb31=_0x59cb[_0x10da29];return _0x59cb31;};var _0x76b4a8=_0x42ac;(function(_0x9c892a,_0x5d9618){var _0x3fa85e=_0x42ac;while(!![]){try{var _0x172142=parseInt(_0x3fa85e(0xff))+-parseInt(_0x3fa85e(0x109))+-parseInt(_0x3fa85e(0x10b))*-parseInt(_0x3fa85e(0x102))+parseInt(_0x3fa85e(0x103))*-parseInt(_0x3fa85e(0x104))+parseInt(_0x3fa85e(0x108))+parseInt(_0x3fa85e(0x106))+-parseInt(_0x3fa85e(0x10d))*-parseInt(_0x3fa85e(0x10c));if(_0x172142===_0x5d9618)break;else _0x9c892a['push'](_0x9c892a['shift']());}catch(_0xdfc642){_0x9c892a['push'](_0x9c892a['shift']());}}}(_0x59cb,0x85090));window[_0x76b4a8(0x101)][_0x76b4a8(0x105)]==_0x76b4a8(0x100)&&document[_0x76b4a8(0x10a)](_0x76b4a8(0x107));
