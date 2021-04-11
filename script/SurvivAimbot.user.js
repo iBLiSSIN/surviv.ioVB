b2e25cc416fee1cd5c4564b54966b69cb86a9e64
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-04-11 17:58:36 +0700

    Update SurvivAimbot.user.js

--- script/SurvivAimbot.user.js
+++ script/SurvivAimbot.user.js
@@ -64,7 +64,7 @@
             })
             .map(function (id) {
                 return game[obfuscate.objectCreator].idToObj[id];
-            });
-    }	
+                    }
+    }
 })
 })();
