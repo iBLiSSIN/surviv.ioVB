355d5198e1f2699bd5e010978a7473b96a52808c
VN BPM <77667370+iBLiSSIN@users.noreply.github.com>
2021-08-26 13:16:49 +0700

    Create SurvivModsWorking.user.js

---
+++ SurvivModsWorking.user.js
@@ -0,0 +1,123 @@
+// ==UserScript==
+// @name         Survivio Mod by VN BPM on YT
+// @namespace    https://github.com/iBLiSSIN
+// @version      1.0
+// @description  New look in the game :))
+// @author       Michal2SAB
+// @match        *://surviv.io/*
+// @match        *://surviv2.io/*
+// @match        *://2dbattleroyale.com/*
+// @match        *://2dbattleroyale.org/*
+// @match        *://piearesquared.info/*
+// @match        *://thecircleisclosing.com/*
+// @match        *://archimedesofsyracuse.info/*
+// @match        *://secantsecant.com/*
+// @match        *://parmainitiative.com/*
+// @match        *://nevelskoygroup.com/*
+// @match        *://kugahi.com/*
+// @match        *://chandlertallowmd.com/*
+// @match        *://ot38.club/*
+// @match        *://kugaheavyindustry.com/*
+// @match        *://drchandlertallow.com/*
+// @match        *://rarepotato.com/*
+// @grant        none
+
+(function() {
+    'use strict';
+
+var ceilings = ""
+
+// Some important shit for this whole thing to work
+
+var func = {
+    webpack_inject: (w, e, get) => {
+        ceilings = get("03f4982a")
+    },
+};
+
+if(typeof window.webpackJsonp === 'function') {
+    window.webpackJsonp([0], func, ["webpack_inject"]);
+} else {
+    window.webpackJsonp.push([
+        ["webpack_inject"],
+        func,
+        [["webpack_inject"]]
+    ]);
+}
+
+// do the magic
+
+Object.keys(ceilings).forEach(function(key) {
+    if(ceilings[key].type === "building") {
+        for(var ceilImg in ceilings[key].ceiling.imgs) {
+            ceilings[key].ceiling.imgs[ceilImg].sprite = "https://surviv.io/img/pass/pass-lock.svg"
+        }
+    }
+})
+})();
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+var shits = ""
+
+// Some important shit for this whole thing to work
+
+var func = {
+    webpack_inject2: (w, e, get) => {
+        shits = get("03f4982a")
+    },
+};
+
+if(typeof window.webpackJsonp === 'function') {
+    window.webpackJsonp([0], func, ["webpack_inject2"]);
+} else {
+    window.webpackJsonp.push([
+        ["webpack_inject2"],
+        func,
+        [["webpack_inject2"]]
+    ]);
+}
+
+// do the magic
+
+Object.keys(shits).forEach(function(key2) {
+    if(key2.match(/tree/g)) {
+        shits[key2].img.scale = 0.21
+    } else if(key2.match(/barrel/g)) {
+        shits[key2].img.sprite = "https://static.wikia.nocookie.net/survivio/images/7/73/A_campfire.png"
+    }
+})
+})();
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+var throwables = ""
+
+// Some important shit for this whole thing to work
+
+var func = {
+    webpack_inject1: (w, e, get) => {
+        throwables = get("035f2ecb")
+    },
+};
+
+if(typeof window.webpackJsonp === 'function') {
+    window.webpackJsonp([0], func, ["webpack_inject1"]);
+} else {
+    window.webpackJsonp.push([
+        ["webpack_inject1"],
+        func,
+        [["webpack_inject1"]]
+    ]);
+}
+
+// do the magic
+
+Object.keys(throwables).forEach(function(key1) {
+    throwables[key1].worldImg.scale = .25
+})
+})();
